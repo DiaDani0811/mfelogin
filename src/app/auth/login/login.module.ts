c69f78aeffe2272fff87bfdf076dfacb49b07a83
dinesh <dinesh.m@neverskip.com>
2024-12-24 14:37:52 +0530

    formgroup aded

--- src/app/auth/login/login.module.ts
+++ src/app/auth/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule, Routes } from '@angular/router';
-import { AppRoutingModule } from '../../app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const route : Routes = [
   {path : '', component : LoginComponent}
@@ -14,7 +14,9 @@ const route : Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(route)
+    RouterModule.forChild(route),
+    ReactiveFormsModule
+    
   ]
 })
 export class LoginModule { }
